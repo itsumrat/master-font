c97783c728abe2e58b0347dd485ce8db15531172
Masum Millat <masummillat@gmail.com>
2020-11-28 00:08:47 +0600

    sidebar

--- src/components/header/HeaderComponent.js
+++ src/components/header/HeaderComponent.js
@@ -1,17 +1,23 @@
 import React, {useState} from "react";
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import logo from '../../assets/img/logo.png';
+import {NavbarToggler} from "reactstrap/es";
 const HeaderComponent = () => {
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const [dropdownOpen2, setDropdownOpen2] = useState(false);
-
+    const [isOpen, setIsOpen] = useState(false);
+    const onMenuToggle = ()=>{
+        setIsOpen(!isOpen);
+        document.getElementById('headerMenu').classList.toggle("show");
+    }
     const toggle = () => setDropdownOpen(prevState => !prevState);
     const toggle2 = () => setDropdownOpen2(prevState => !prevState);
     return(
         <div id="MainShope">
             <div className="TopHeader">
                 <div className="Mlogo"><img src={logo}/></div>
-                <div className="HeadrMenu">
+                <span onClick={onMenuToggle} className="material-icons toggler">{isOpen ? 'close' : 'menu'}</span>
+                <div id="headerMenu" className="HeadrMenu">
                     <div className="MenuItem">כל הפונטים</div>
                     <div className="MenuItem"><a href="#">מבצעים</a></div>
                     <div className="MenuItem">
@@ -45,14 +51,16 @@ const HeaderComponent = () => {
                             </DropdownMenu>
                         </Dropdown>
                     </div>
+                    <div  className="HeadrIcons" >
+
+                        <ul>
+                            <li><a href="#"><span className="material-icons">favorite</span></a></li>
+                            <li><a href="#"><span className="material-icons">lock</span></a></li>
+                            <li><a href="#"><span className="material-icons">person</span></a></li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="HeadrIcons">
-                    <ul>
-                        <li><a href="#"><span className="material-icons">favorite</span></a></li>
-                        <li><a href="#"><span className="material-icons">lock</span></a></li>
-                        <li><a href="#"><span className="material-icons">person</span></a></li>
-                    </ul>
-                </div>
+
             </div>
         </div>
     );
